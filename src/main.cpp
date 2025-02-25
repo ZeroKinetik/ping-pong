a7dd85e3484655470eff860bf72f7cde091e1571
ZeroKinetk <hemansakkthivelms14@gmail.com>
2025-02-25 20:14:08 +0530

    added objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
-
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+    sf::CircleShape circle;
+    circle.setRadius(6);
+    circle.setFillColor(sf::Color::White);
+    circle.setOutlineThickness(2);
+    circle.setPosition({390.0f, 290.0f});
+    sf::RectangleShape leftpaddle;
+    leftpaddle.setSize(sf::Vector2f(10, 100));
+    leftpaddle.setFillColor(sf::Color::White);
+    leftpaddle.setOutlineThickness(2);
+    leftpaddle.setPosition({10.0f, static_cast<float>(window.getSize().y) / 2 - 50});
+    sf::RectangleShape rightpaddle;
+    rightpaddle.setSize(sf::Vector2f(10, 100));
+    rightpaddle.setFillColor(sf::Color::White);
+    rightpaddle.setOutlineThickness(2);
+    rightpaddle.setPosition({static_cast<float>(window.getSize().x) - 20, static_cast<float>(window.getSize().y) / 2 - 50});
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
-
-        window.clear();
+        window.clear(sf::Color::Black);
+        window.draw(circle);
+        window.draw(leftpaddle);
+        window.draw(rightpaddle);
         window.display();
     }
 }
